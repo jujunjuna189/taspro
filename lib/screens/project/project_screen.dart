d1e314cd5dff5a06477a84796c4e33f60ed446e7
jujunjuna189 <53271616+jujunjuna189@users.noreply.github.com>
2022-06-11 16:10:31 +0700

    Update project_screen.dart

--- lib/screens/project/project_screen.dart
+++ lib/screens/project/project_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:taspro/utils/sizes.dart';
+import 'package:taspro/widgets/button/previouse_back_button.dart';
 import 'package:taspro/widgets/sleek/sleek_progress.dart';
+import 'package:taspro/widgets/text/medium_text.dart';
+import 'package:taspro/widgets/text/normal_text.dart';
 
 class ProjectScreen extends StatefulWidget {
   const ProjectScreen({Key? key}) : super(key: key);
@@ -14,15 +17,38 @@ class _ProjectScreenState extends State<ProjectScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
-        padding: EdgeInsets.only(
-            left: Sizes.intense.screenHorizontal(context) * 5,
-            right: Sizes.intense.screenHorizontal(context) * 5,
-            top: Sizes.intense.screenHorizontal(context) * 10,
-            bottom: Sizes.intense.screenHorizontal(context) * 10
-        ),
         physics: const ScrollPhysics(),
-        children: const [
-          SleekProgress(),
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              PreviouseBackButton(
+                onTap: ((){
+                  Navigator.of(context).pop();
+                }),
+              ),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+                left: Sizes.intense.screenHorizontal(context) * 5,
+                right: Sizes.intense.screenHorizontal(context) * 5,
+                bottom: Sizes.intense.screenHorizontal(context) * 10
+            ),
+            child: Column(
+              children: [
+                const SleekProgress(),
+                SizedBox(height: Sizes.intense.screenVertical(context) * 5,),
+                const MediumText(text: "Judul Tugas Utama"),
+                SizedBox(height: Sizes.intense.screenVertical(context) * 5,),
+                Row(
+                  children: [
+                    
+                  ],
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
